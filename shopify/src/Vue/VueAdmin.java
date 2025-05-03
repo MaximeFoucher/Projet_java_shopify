ee5c3f2762b2dcc52ac9a52403182f3d07a5ad91
Justin Real <justin.real95@gmail.com>
2025-05-03 11:45:18 +0200

    Vues admin et client terminées

--- shopify/src/Vue/VueAdmin.java
+++ shopify/src/Vue/VueAdmin.java
@@ -1,4 +1,17 @@
 package Vue;
 
+import Modele.Admin;
+import java.util.ArrayList;
+
 public class VueAdmin {
+    public void afficherAdmin(Admin admin) {
+        System.out.println("\nId admin:" + admin.getId() + "\nNom admin:" + admin.getName()
+                + "\nEmail admin:" + admin.getEmail() + "\nMdp admin:" + admin.getMdp());
+    }
+
+    public void afficherListeadmin(ArrayList<Admin> admins) {
+        for (Admin admin : admins) {
+            afficherAdmin(admin);
+        }
+    }
 }
--- shopify/src/Vue/VueClient.java
+++ shopify/src/Vue/VueClient.java
@@ -1,4 +1,17 @@
 package Vue;
 
+import Modele.Client;
+import java.util.ArrayList;
+
 public class VueClient {
+    public void afficherClient(Client client) {
+        System.out.print("\nId client:" + client.getId() + "\nNom client:" + client.getName()
+        + "\nEmail client:" + client.getEmail() + "\nMdp client:" + client.getMdp());
+    }
+
+    public void afficherListeClient(ArrayList<Client> clients) {
+        for (Client client : clients) {
+            afficherClient(client);
+        }
+    }
 }
